a90bb1473753213eec7879f270440510125b66d8
terrielama <60408333+terrielama@users.noreply.github.com>
2021-12-17 22:07:28 +0100

    Create joueur.rs

---
+++ src/joueur.rs
@@ -0,0 +1,8 @@
+use bevy::{core::FixedTimestep, prelude::*};
+
+use crate::{
+	FromJoueur, Laser, Materials, Joueur, JoueurReadyFire, JoueurState, Speed, WinSize,
+	JOUEUR_RESPAWN_DELAY, SCALE, TIME_STEP,
+};
+
+pub struct JoueurPlugin;
