3d5a266959f7c5f28f4f55cbe9e177146c5d2ede
terrielama <60408333+terrielama@users.noreply.github.com>
2021-12-18 13:06:47 +0100

    Update enemies.rs

--- src/enemies.rs
+++ src/enemies.rs
@@ -8,3 +8,26 @@ use bevy::{core::FixedTimestep, prelude::*};
 use rand::{thread_rng, Rng};
 
 pub struct EnemiesPlugin;
+
+
+//placement au depart
+// Component
+#[derive(Default, Clone)]
+struct Formation {
+	start: (f32, f32),
+	radius: (f32, f32),
+	offset: (f32, f32),
+	angle: f32,
+	group_id: u32,
+}
+
+
+
+
+
+
+
+
+
+
+
